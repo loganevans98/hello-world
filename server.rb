6f57f2d56247f2f657ea78e28702b57da69ca34c
Logan Evans <loganevans98@gmail.com>
2016-07-11 15:22:42 -0700

    Search Giphy

--- server.rb
+++ server.rb
@@ -1,4 +1,7 @@
 require 'sinatra'
+require 'http'
+require 'uri'
+
 
 get '/' do
   erb :index
@@ -9,6 +12,16 @@ get '/test' do
 end
 
 get '/giphy' do
-	erb :giphy
+	erb :giphy, locals: { query: '', image_url: '' }
 end
 
+post '/search_giphy' do
+	query = params[:query]
+	escaped_query = URI.escape(query)
+	response = HTTP.get("http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=#{escaped_query}")
+
+	erb :giphy, locals: {
+							query: query,
+							image_url: response.parse["data"]["image_url"]
+						}
+end
