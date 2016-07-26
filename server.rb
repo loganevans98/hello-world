cdff637a2c03352125a638441cb2332f56855873
Sean Durham <sean@sdurham.net>
2016-07-26 14:38:45 -0700

    Protect admin routes with basic auth

--- server.rb
+++ server.rb
@@ -8,6 +8,20 @@ require './models/query'
 require 'obscenity'
 require './config/pokemon_names'
 
+helpers do
+  def protected!
+    return if authorized?
+    headers['WWW-Authenticate'] = 'Basic realm="Restricted Area"'
+    halt 401, "Not authorized\n"
+  end
+
+  def authorized?
+    @auth ||=  Rack::Auth::Basic::Request.new(request.env)
+    puts ENV['ADMIN_USERNAME'], ENV['ADMIN_PASSWORD']
+    @auth.provided? and @auth.basic? and @auth.credentials and @auth.credentials == [ENV['ADMIN_USERNAME'], ENV['ADMIN_PASSWORD']]
+  end
+end
+
 #-------------------#
 # Home              #
 #-------------------#
@@ -58,16 +72,19 @@ get '/queries/:id' do
 end
 
 get '/admin' do
-  # Make sure they have permission to view admin page
+  protected!
+
   @queries = Query.all.order(created_at: :desc)
   erb 'admin/index'.to_sym
 end
 
 delete '/results/:id' do
+  protected!
   Query.destroy_result(params[:id])
 end
 
 delete '/queries/:id' do
+  protected!
   query = Query.find params[:id]
   query.destroy!
 end
